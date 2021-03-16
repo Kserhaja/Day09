b1fbcc189ea470b27b9d0e85948788e42b2e1db2
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-16 21:56:05 +0100

    return true boolean

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+
+function myTrueBoolean() {
+    // Only change code below this line.
+        return true 
+                 
+        
+    
+    // Only change code above this line.
+    
+}
+
+
+
+console.log(myTrueBoolean());
+
+module.exports = myTrueBoolean;

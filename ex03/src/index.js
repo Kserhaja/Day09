508e1b1fd8e53e135d2ef6173066f0fd42d6d44a
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-17 00:44:18 +0100

    Equal, not equal

---
+++ ex03/src/index.js
@@ -0,0 +1,31 @@
+const myFunction = require("../../ex03/src");
+
+function myEqualFunction(num) {
+    
+        if(num === 23) {
+       // Only change code below this line.
+       return "Equal";
+        };
+        return "Not Equal";
+   // Only change code above this line.
+    
+}
+
+
+
+
+myEqualFunction(5);
+myEqualFunction(23);
+myEqualFunction("23");
+myEqualFunction('23');
+myEqualFunction("text");
+
+
+console.log(myEqualFunction(5));
+console.log(myEqualFunction(23));
+console.log(myEqualFunction("23"));
+console.log(myEqualFunction('23'));
+console.log(myEqualFunction("text"));
+
+
+module.exports = myFunction;

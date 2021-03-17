f21fbc0572ad204882a0fd6b542d57042971a34a
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-17 01:22:38 +0100

    Logic order Else if

---
+++ ex10/src/index.js
@@ -0,0 +1,27 @@
+function logicOrder(num) {
+    // Only change code below this line.
+    if(num < 50) { return "Less than 50"}
+   
+    else if (num < 100) { return "Less than 100";
+}
+    else { return "Greater than or equal to 100";
+    }
+    
+    
+    
+// Only change code above this line.
+
+}
+
+logicOrder();
+
+
+
+console.log(logicOrder(28));
+console.log(logicOrder(60));
+console.log(logicOrder(110));
+
+
+
+
+module.exports = logicOrder;

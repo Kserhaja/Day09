216e82a452cdd821302f2b335d0edbcd2d3dcfc2
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-17 01:16:23 +0100

    bigger or smaller Else

---
+++ ex09/src/index.js
@@ -0,0 +1,27 @@
+function functionElse(num) {
+    // Only change code below this line.
+    if(num <= 5)
+   
+    return "5 or smaller";
+    else { (num > 5)
+    return "Bigger than 5";
+    }
+    
+    
+    
+// Only change code above this line.
+
+}
+
+functionElse();
+
+
+
+console.log(functionElse(4));
+console.log(functionElse(5));
+console.log(functionElse(6));
+console.log(functionElse(10));
+
+
+
+module.exports = functionElse;
